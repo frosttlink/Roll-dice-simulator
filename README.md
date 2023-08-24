300cec8ee050976e44c76d15d48a3d044c8e0f7c
Léo Alves <leolorena.alves@gmail.com>
2023-08-23 22:21:39 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+<h1 align="center"> 🎲 Roll Dice Simulator 🎲 </h1>
+
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-Instruções de uso">Instruções de uso</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="projeto Habits" src=".github/preview.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Git e Github
+
+
+## 💻 Projeto
+
+O projeto Roll dice simulator serve para aqueles que querem rolar seus diversos dados de rpg, mas não tem um em casa.
+ Para isso foi feito um site onde pode rolar todos os dados, e até personalizar o número de lados que tenha o devido dado.
+
+## 📌 Instruções de uso
+
+Para o uso correto do Roll dice Simulator, você tera duas alternativas.
+
+
+### 🖐 Manual 
+
+O uso manual funciona da seguinte forma.
+Copie os dados dos arquivos .html .css .js, assim você terá clonado o código e poderá usar o Roll Dice Simulator
+
+### 💻 Online
+
+O uso online você poderá acessar por [ESTE LINK](https://leoclink77.github.io/Roll-dice-simulator/)
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+Feito com ♥ by [Frost](https://bento.me/frost)
---
+++ script.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const d4Btn = document.getElementById("d4");
+  const d6Btn = document.getElementById("d6");
+  const d8Btn = document.getElementById("d8");
+  const d10Btn = document.getElementById("d10");
+  const d12Btn = document.getElementById("d12");
+  const d20Btn = document.getElementById("d20");
+  const customBtn = document.getElementById("custom");
+  const customSidesInput = document.getElementById("customSides");
+  const resultDiv = document.getElementById("result");
+
+  function rollDice(sides) {
+      return Math.floor(Math.random() * sides) + 1;
+  }
+
+  function animateResult(result) {
+      resultDiv.style.opacity = 0;
+      resultDiv.textContent = "Result: " + result;
+      setTimeout(function() {
+          resultDiv.style.opacity = 1;
+      }, 200);
+  }
+
+  d4Btn.addEventListener("click", function() {
+      const result = rollDice(4);
+      animateResult(result);
+  });
+
+  d6Btn.addEventListener("click", function() {
+      const result = rollDice(6);
+      animateResult(result);
+  });
+
+  d8Btn.addEventListener("click", function() {
+      const result = rollDice(8);
+      animateResult(result);
+  });
+
+  d10Btn.addEventListener("click", function() {
+      const result = rollDice(10);
+      animateResult(result);
+  });
+
+  d12Btn.addEventListener("click", function() {
+      const result = rollDice(12);
+      animateResult(result);
+  });
+
+  d20Btn.addEventListener("click", function() {
+      const result = rollDice(20);
+      animateResult(result);
+  });
+
+  customBtn.addEventListener("click", function() {
+      const customSides = parseInt(customSidesInput.value);
+      if (!isNaN(customSides) && customSides > 0) {
+          const result = rollDice(customSides);
+          animateResult(result);
+      } else {
+          resultDiv.textContent = "Invalid input!";
+      }
+  });
+});
+
